c202e46292323de92a500348438676f1d8d5efd5
mercari-komamiya <tmp-h-komamiya@mercari.com>
2023-03-23 19:01:59 +0900

    astro: add meta data

---
+++ astro/test-blog/src/lib/constants.js
@@ -0,0 +1,9 @@
+export const siteMeta = {
+  siteTitle: "Tech Flow.",
+  siteDesc: "Tech Flow の技術ブログサイト",
+  siteUrl: "https://**************",
+  siteType: "website",
+  siteLocale: "ja_JP",
+  siteIcon: "/favicon.png",
+  siteImg: "/ogp.jpg",
+};
