4990ba774fdaa43d2b4ed379675e621ebd411dc6
mercari-komamiya <tmp-h-komamiya@mercari.com>
2023-03-30 18:42:44 +0900

    three: add renderer

--- frontend/threejs/tutorial/src/main.ts
+++ frontend/threejs/tutorial/src/main.ts
@@ -1,9 +1,15 @@
 import * as THREE from "three";
 
-let scene, camera;
+let scene, camera, renderer;
 
 // シーンの追加
 scene = new THREE.Scene();
 
 // カメラの追加：PerspectiveCamera(視野角, アスペクト比, 開始距離, 終了距離)
 camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// レンダラーの追加
+renderer = new THREE.WebGLRenderer();
+document.body.appendChild(renderer.domElement);  // body要素内に表示
+renderer.setSize(window.innerWidth, window.innerHeight);  // 画面いっぱいに表示
+renderer.render(scene, camera);
