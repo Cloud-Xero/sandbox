7049596528f2bd40d0c8cc3b001968dc530bb1d3
mercari-komamiya <tmp-h-komamiya@mercari.com>
2023-03-30 18:36:22 +0900

    three: add camera

--- frontend/threejs/tutorial/src/main.ts
+++ frontend/threejs/tutorial/src/main.ts
@@ -1,5 +1,9 @@
 import * as THREE from "three";
 
-let scene;
+let scene, camera;
 
+// シーンの追加
 scene = new THREE.Scene();
+
+// カメラの追加：PerspectiveCamera(視野角, アスペクト比, 開始距離, 終了距離)
+camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
