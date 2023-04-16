4c90631ee24db207b754de94e37df6b3458d1ad8
Cloud-Xero <hiropon201511@gmail.com>
2023-04-16 10:58:34 +0900

    astro: set tailwind

--- astro/test-blog/astro.config.mjs
+++ astro/test-blog/astro.config.mjs
@@ -4,15 +4,19 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
-const {
-  siteUrl
-} = siteMeta;
-
+import tailwind from "@astrojs/tailwind";
+const { siteUrl } = siteMeta;
 
 // https://astro.build/config
 export default defineConfig({
   site: siteUrl,
-  integrations: [image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), prefetch(), sitemap()]
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    prefetch(),
+    sitemap(),
+    tailwind({ config: { applyBaseStyles: false } }),
+  ],
 });
