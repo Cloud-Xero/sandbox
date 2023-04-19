51b2d3169d50e3d4cbade598497a98a5cf10b0ec
mercari-komamiya <tmp-h-komamiya@mercari.com>
2023-04-19 18:29:41 +0900

    tf: fix machine type & zone

--- terraform/gcp/basis/main.tf
+++ terraform/gcp/basis/main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "default" {
   name         = "test"
-  machine_type = "f1_micro"
-  zone         = "asia-northeast1-c"
+  machine_type = "e2-medium"
+  zone         = "asia-northeast1-b"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
