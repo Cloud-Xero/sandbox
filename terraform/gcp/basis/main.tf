38295bbe09319eb12476c8ade549c639c26e093a
Cloud-Xero <hiropon201511@gmail.com>
2023-04-20 14:29:15 +0900

    tf: use data source

--- terraform/gcp/basis/main.tf
+++ terraform/gcp/basis/main.tf
@@ -1,33 +1,13 @@
-variable "service_name" {
-  description = "A name of the service"
-  type        = string
-  default     = "default"
-}
-
-variable "environment" {
-  description = "An environment of the service"
-  type        = string
-  default     = "dev"
-
-  validation {
-    condition     = contains(["dev", "stg", "prod"], var.environment)
-    error_message = "The environment must be dev, stg or prod."
-  }
-}
-
-locals {
-  common_labels = {
-    service     = var.service_name
-    environment = var.environment
-  }
+data "google_compute_zones" "available" {
+  region = "asia-northeast1"
+  status = "UP"
 }
 
 resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "e2-medium"
-  zone         = "asia-northeast1-b"
+  zone         = data.google_compute_zones.available.names[0]
 
-  labels = local.common_labels
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
