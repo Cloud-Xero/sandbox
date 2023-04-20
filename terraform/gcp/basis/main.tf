bad83b089811a2b35fc86cd8b1d5a95789f9701b
mercari-komamiya <tmp-h-komamiya@mercari.com>
2023-04-20 10:38:00 +0900

    tf: use variable

--- terraform/gcp/basis/main.tf
+++ terraform/gcp/basis/main.tf
@@ -1,7 +1,18 @@
+variable "compute_instance_zone" {
+  description = "A zone used in compute instance"
+  type        = string
+  default     = "asia-northeast1-b"
+
+  validation {
+    condition     = contains(["asia-northeast1-a", "asia-northeast1-b", "asia-northeast1-c"], var.compute_instance_zone)
+    error_message = "The compute_instance_zone must be in asia-northeast1 region."
+  }
+}
+
 resource "google_compute_instance" "default" {
   name         = "test3"
   machine_type = "e2-medium"
-  zone         = "asia-northeast1-b"
+  zone         = var.compute_instance_zone
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -10,9 +21,5 @@ resource "google_compute_instance" "default" {
 
   network_interface {
     network = "default"
-  }
-
-  lifecycle {
-    ignore_changes = [name]
   }
 }
