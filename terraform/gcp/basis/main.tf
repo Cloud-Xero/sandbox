89aa7bfbedc5522000eb8f2eaaead2722f461dc3
Cloud-Xero <hiropon201511@gmail.com>
2023-04-21 23:52:06 +0900

    tf: explicit versions

--- terraform/gcp/basis/main.tf
+++ terraform/gcp/basis/main.tf
@@ -1,13 +1,19 @@
-variable "vm_enable" {
-  type    = bool
-  default = false
+terraform {
+  required_version = "~> 1.2"
+  required_providers {
+    google = ">= 4.32.0"
+  }
 }
-resource "google_compute_instance" "default" {
-  count = var.vm_enable ? 1 : 0
 
-  name         = "test"
+provider "google" {
+  # [YOUR PROJECT]を自身のGCPプロジェクト名に置き換える
+  project = "[YOUR PROJECT]"
+  zone    = "asia-northeast1-b"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test2"
   machine_type = "e2-medium"
-  zone         = "asia-northeast1-b"
 
   boot_disk {
     initialize_params {
@@ -18,4 +24,14 @@ resource "google_compute_instance" "default" {
   network_interface {
     network = "default"
   }
+}
+
+output "project" {
+  description = "Names of project"
+  value       = google_compute_instance.default.project
+}
+
+output "zone" {
+  description = "Names of zone"
+  value       = google_compute_instance.default.zone
 }
