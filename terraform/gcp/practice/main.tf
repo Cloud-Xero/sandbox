0285faab682ae11fecaefdc052008a3dd1d9aeb7
Cloud-Xero <hiropon201511@gmail.com>
2023-04-28 11:21:36 +0900

    tf: manage instance group

--- terraform/gcp/practice/main.tf
+++ terraform/gcp/practice/main.tf
@@ -35,3 +35,14 @@ resource "google_compute_region_instance_group_manager" "default" {
   base_instance_name = "mig"
   target_size        = 6
 }
+
+resource "google_compute_region_autoscaler" "default" {
+  name = "default"
+
+  target = google_compute_region_instance_group_manager.default.self_link
+
+  autoscaling_policy {
+    max_replicas = 10
+    min_replicas = 6
+  }
+}
