8cb8118d6812ec227b563b303e9d8a06bd385f17
Cloud-Xero <hiropon201511@gmail.com>
2023-05-15 18:35:07 +0900

    tf: add two firewall

--- terraform/gcp/practice/main.tf
+++ terraform/gcp/practice/main.tf
@@ -53,3 +53,29 @@ resource "google_compute_health_check" "mig_health_check" {
     port = 80
   }
 }
+
+resource "google_compute_firewall" "mig_health_check" {
+  name    = "health-check"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = [80]
+  }
+
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+  target_tags   = ["allow-service"]
+}
+
+resource "google_compute_firewall" "default_ssh" {
+  name    = "default-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = [22]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-ssh"]
+}
