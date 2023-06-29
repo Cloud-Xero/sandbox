ca0aae13e482b09a061759ecce2a46b7203dd8f3
Cloud-Xero <hiropon201511@gmail.com>
2023-06-29 11:45:27 +0900

    chore: add files for lesson

---
+++ typescript/0627/vite.config.js
@@ -0,0 +1,11 @@
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        sub: "./great_kanto_earthquake.html",
+      },
+    },
+  },
+  base: "",
+};
