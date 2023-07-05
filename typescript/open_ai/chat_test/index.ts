73fd0cfcc885c33ddd56040bb3b0cad61b8971e3
Cloud-Xero <hiropon201511@gmail.com>
2023-07-05 16:15:18 +0900

    chore: use open ai api

---
+++ typescript/open_ai/chat_test/index.ts
@@ -0,0 +1,17 @@
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const completionFunction = async () => {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "こんにちは" }],
+  });
+  console.log(completion.data.choices[0].message);
+};
+
+completionFunction();
