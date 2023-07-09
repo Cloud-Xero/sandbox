f70bb5b3745d13b1343b72f5da4c6a5b7ad4dfc8
Cloud-Xero <hiropon201511@gmail.com>
2023-07-09 20:31:01 +0900

    feat: add function to list

--- typescript/open_ai/function_calling/index.js
+++ typescript/open_ai/function_calling/index.js
@@ -34,6 +34,20 @@ const functions = [
         },
         required: ["location", "date"],
     },
+    {
+        name: "get_capital",
+        description: "指定された国の首都を取得する",
+        parameters: {
+            type: "object",
+            properties: {
+                country: {
+                    type: "string",
+                    description: "国名。e.g. フランス",
+                },
+            },
+        },
+        required: ["country"],
+    },
 ];
 const messages = [
     {
@@ -41,39 +55,63 @@ const messages = [
         content: "明日（2023/7/8）の東京の天気わかりますか？",
     },
 ];
+const messages2 = [
+    {
+        role: "user",
+        content: "日本の首都はどこですか？",
+    },
+];
 // 外部APIの定義（今回はモック）
 const getWeather = (location, date) => {
     return { date, location, weather: "晴れ" };
+};
+// 外部APIの定義（今回はモック）
+const getCapital = (country) => {
+    return { country, capital: "東京都" };
 };
 const completionFunction = () => __awaiter(void 0, void 0, void 0, function* () {
     // API実行
     const completion = yield openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: messages,
+        messages: messages2,
         functions,
     });
     // console.log(completion.data.choices[0]);
     if (completion.data.choices[0].finish_reason === "function_call") {
+        let res;
         // コールするfunction
         const functionCall = completion.data.choices[0].message.function_call;
-        // 引数をparse
-        const { location, date } = JSON.parse(functionCall.arguments);
-        // 引数を使用して外部APIを実行
-        const weather = yield getWeather(location, date);
-        console.log(weather);
+        if (functionCall.name === "get_weather") {
+            // 引数をparse
+            const { location, date } = JSON.parse(functionCall.arguments);
+            // 引数を使用して外部APIを実行
+            res = yield getWeather(location, date);
+            // console.log(res); // { date: '2023/07/08', location: '東京都', weather: '晴れ' }
+        }
+        else if (functionCall.name === "get_capital") {
+            // 引数をparse
+            const { country } = JSON.parse(functionCall.arguments);
+            // 引数を使用して外部APIを実行
+            res = yield getCapital(country);
+            // console.log(res);  // { country: '日本', capital: '東京都' }
+        }
+        else {
+            throw new Error(`Unknown function: ${functionCall}`);
+        }
         // messages配列に新たなオブジェクトの要素を追加
-        messages.push({
+        messages2.push({
             role: "function",
-            content: JSON.stringify(weather),
+            content: JSON.stringify(res),
             name: functionCall.name,
         });
         // API実行
-        const res = yield openai.createChatCompletion({
+        const res2 = yield openai.createChatCompletion({
             model: "gpt-3.5-turbo",
-            messages,
+            messages: messages2,
             functions,
         });
-        console.log(res.data.choices[0]);
+        // API側に回答を生成してもらう（ただし、リクエスト回数が1回増えてしまう）
+        console.log(res2.data.choices[0]);
     }
 });
 completionFunction();
--- typescript/open_ai/function_calling/index.ts
+++ typescript/open_ai/function_calling/index.ts
@@ -32,6 +32,20 @@ const functions = [
     },
     required: ["location", "date"],
   },
+  {
+    name: "get_capital",
+    description: "指定された国の首都を取得する",
+    parameters: {
+      type: "object",
+      properties: {
+        country: {
+          type: "string",
+          description: "国名。e.g. フランス",
+        },
+      },
+    },
+    required: ["country"],
+  },
 ];
 
 const messages: Messages = [
@@ -41,49 +55,71 @@ const messages: Messages = [
   },
 ];
 
+const messages2: Messages = [
+  {
+    role: "user",
+    content: "日本の首都はどこですか？",
+  },
+];
+
 // 外部APIの定義（今回はモック）
 const getWeather = (location: string, date: string) => {
   return { date, location, weather: "晴れ" };
+};
+
+// 外部APIの定義（今回はモック）
+const getCapital = (country: string) => {
+  return { country, capital: "東京都" };
 };
 
 const completionFunction = async () => {
   // API実行
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: messages,
+    messages: messages2,
     functions,
   });
   // console.log(completion.data.choices[0]);
 
   if (completion.data.choices[0].finish_reason === "function_call") {
+    let res;
     // コールするfunction
     const functionCall = completion.data.choices[0].message.function_call;
 
-    // 引数をparse
-    const { location, date } = JSON.parse(functionCall.arguments);
+    if (functionCall.name === "get_weather") {
+      // 引数をparse
+      const { location, date } = JSON.parse(functionCall.arguments);
 
-    // 引数を使用して外部APIを実行
-    const weather = await getWeather(location, date);
-    console.log(weather); // { date: '2023/07/08', location: '東京都', weather: '晴れ' }
+      // 引数を使用して外部APIを実行
+      res = await getWeather(location, date);
+      // console.log(res); // { date: '2023/07/08', location: '東京都', weather: '晴れ' }
+    } else if (functionCall.name === "get_capital") {
+      // 引数をparse
+      const { country } = JSON.parse(functionCall.arguments);
 
-    // `${date}の${location}天気は${weathre}です`　// ex 2023/07/08の東京の天気は晴れです.
+      // 引数を使用して外部APIを実行
+      res = await getCapital(country);
+      // console.log(res);  // { country: '日本', capital: '東京都' }
+    } else {
+      throw new Error(`Unknown function: ${functionCall}`);
+    }
 
     // messages配列に新たなオブジェクトの要素を追加
-    messages.push({
+    messages2.push({
       role: "function",
-      content: JSON.stringify(weather),
+      content: JSON.stringify(res),
       name: functionCall.name,
     });
 
     // API実行
-    const res = await openai.createChatCompletion({
+    const res2 = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages,
+      messages: messages2,
       functions,
     });
 
     // API側に回答を生成してもらう（ただし、リクエスト回数が1回増えてしまう）
-    console.log(res.data.choices[0]);
+    console.log(res2.data.choices[0]);
   }
 };
 
