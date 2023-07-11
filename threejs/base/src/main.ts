3dcf695c141bbe99fe189765d2b7b1239b3fc191
Cloud-Xero <hiropon201511@gmail.com>
2023-07-11 16:15:39 +0900

    feat: replace texture

--- threejs/base/src/main.ts
+++ threejs/base/src/main.ts
@@ -7,18 +7,29 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true, opacity: 0.5, wireframe: true });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.PlaneGeometry(20, 10);
 
-camera.position.z = 5;
+const texLoader = new THREE.TextureLoader();
+const texture1 = await texLoader.loadAsync("../landscape.avif");
+const texture2 = await texLoader.loadAsync("../sand.avif");
+
+const material = new THREE.MeshBasicMaterial({ map: texture1 });
+
+setTimeout(() => {
+  material.map = texture2;
+}, 2000);
+
+const plane = new THREE.Mesh(geometry, material);
+
+scene.add(plane);
+
+camera.position.z = 30;
 
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  plane.rotation.x += 0.01;
+  plane.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
