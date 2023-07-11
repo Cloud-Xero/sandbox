174c55507c65fb63a9bbebc3f1ce78a0fb918723
Cloud-Xero <hiropon201511@gmail.com>
2023-07-11 19:37:27 +0900

    feat: use orbitControl

--- threejs/base/src/main.ts
+++ threejs/base/src/main.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -7,30 +8,19 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.PlaneGeometry(20, 10);
-
-const texLoader = new THREE.TextureLoader();
-const texture1 = await texLoader.loadAsync("../landscape.avif");
-const texture2 = await texLoader.loadAsync("../sand.avif");
-
-const material = new THREE.MeshBasicMaterial({ map: texture1 });
-
-setTimeout(() => {
-  material.map = texture2;
-}, 2000);
+const geometry = new THREE.TorusGeometry(7, 3, 200, 20);
+const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 
 const plane = new THREE.Mesh(geometry, material);
 
 scene.add(plane);
 
+new OrbitControls(camera, renderer.domElement);
+
 camera.position.z = 30;
 
 function animate() {
   requestAnimationFrame(animate);
-
-  plane.rotation.x += 0.01;
-  plane.rotation.y += 0.01;
-
   renderer.render(scene, camera);
 }
 animate();
