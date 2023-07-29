4e3e50576365f1571fa10550638062a5679f9e3d
Cloud-Xero <hiropon201511@gmail.com>
2023-07-29 15:30:40 +0900

    types: add WebGPU

---
+++ threejs/threads/src/types.d.ts
@@ -0,0 +1,10 @@
+// three-addons-capabilities-WebGPU.d.ts
+declare module "three/addons/capabilities/WebGPU.js" {
+  const WebGPU: any;
+  export = WebGPU;
+}
+
+declare module "three/addons/renderers/webgpu/WebGPURenderer.js" {
+  const WebGPURenderer: any;
+  export = WebGPURenderer;
+}
