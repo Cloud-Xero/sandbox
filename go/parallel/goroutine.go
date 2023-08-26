7ea594ce0023adb0e30901d902b4870326e68f33
Cloud-Xero <hiropon201511@gmail.com>
2023-08-26 17:10:18 +0900

    feat: use sync.WaitGroup

--- go/parallel/goroutine.go
+++ go/parallel/goroutine.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		fmt.Println("goroutine invoked")
 	}()
-
+	wg.Wait()
 	fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
 	fmt.Println("main func finish")
 }
