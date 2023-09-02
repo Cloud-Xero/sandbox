cdbf82a4ac835fc938cbca50c58023626b92c4c4
Cloud-Xero <hiropon201511@gmail.com>
2023-09-02 17:01:46 +0900

    docs: add comment

--- go/notion/update/main.go
+++ go/notion/update/main.go
@@ -8,13 +8,13 @@ func main() {
 	if monthlyFlag {
 
 		// Monthly Execution
-		UpdateSelectedMonthlyColor()
+		// UpdateSelectedMonthlyColor()  // DBのテンプレートを直接変更することはできないので現状使用不可（ページ = 行単位で行う必要がある）
 		res := GetSelectedMonthData()
 		UpdateSelectedMonthData(res)
 	}
 
 	// Weekly Execution
-	UpdateSelectedWeeklyColor()
+	// UpdateSelectedWeeklyColor()  // DBのテンプレートを直接変更することはできないので現状使用不可（ページ = 行単位で行う必要がある）
 	resBody := GetSelectedWeekData()
 	UpdateSelectedWeekData(resBody)
 }
