cbab2a05e6bd5ee6f400967d0d69982d6227d4b6
Cloud-Xero <hiropon201511@gmail.com>
2023-10-21 17:16:20 +0900

    feat: draw 2 points ( sky tree & tokyo tower

--- mapbox/tutorial/src/main.ts
+++ mapbox/tutorial/src/main.ts
@@ -1,11 +1,22 @@
 import "./style.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+import { geoJsonPoints, symbols } from "../data/GeoJson-Point";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v12", // style URL
   center: [139.8107, 35.710063], // 東京スカイツリー [経度, 緯度]
-  zoom: 9, // starting zoom
+  zoom: 11, // starting zoom
+});
+
+map.on("load", () => {
+  map.loadImage("https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png", (error, image) => {
+    if (error) throw error;
+    map.addImage("custom-marker", image as any);
+    // Add a GeoJSON source with 2 points
+    map.addSource("points", geoJsonPoints as mapboxgl.AnySourceData);
+    map.addLayer(symbols as mapboxgl.AnyLayer);
+  });
 });
