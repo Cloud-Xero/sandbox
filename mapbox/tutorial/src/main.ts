8fd101ae871b6c0381d868e1876f211736d0b313
Cloud-Xero <hiropon201511@gmail.com>
2023-10-21 16:48:14 +0900

    refactor

--- mapbox/tutorial/src/main.ts
+++ mapbox/tutorial/src/main.ts
@@ -1,34 +1,11 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 
-mapboxgl.accessToken = "pk.eyJ1Ijoiay1oaXJva2kiLCJhIjoiY2xudThpdHA1MDk0NzJqbGhpbmszZ251eiJ9.OD96NST8JdXbAh40l90--g";
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v12", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
+  center: [139.8107, 35.710063], // 東京スカイツリー [経度, 緯度]
   zoom: 9, // starting zoom
 });
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
