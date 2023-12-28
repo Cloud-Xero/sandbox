8c9e4e7e482048f436d3cbc06ff385e275412510
Cloud-Xero <hrk.komamiya@gmail.com>
2023-12-28 18:17:47 +0900

    feat: get articles by using News API

--- react-native/first/App.js
+++ react-native/first/App.js
@@ -1,9 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import { FlatList, SafeAreaView, StyleSheet } from "react-native";
 import { ListItem } from "./components/ListItem";
-import articles from "./dummies/articles.json";
+import { useEffect, useState } from "react";
+// import dummyArticles from "./dummies/articles.json";
+import axios from "axios";
+import Constants from "expo-constants";
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.expoConfig.extra.newsApiKey}`;
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      // console.log(response.data.articles);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
   const items = articles.map((article, index) => (
     <ListItem
       imageUrl={article.urlToImage}
