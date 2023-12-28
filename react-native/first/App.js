973883d6b62b6cfe63b0c708d435a06ae85669e6
Cloud-Xero <hrk.komamiya@gmail.com>
2023-12-28 13:00:50 +0900

    feat: create UI components by using props

--- react-native/first/App.js
+++ react-native/first/App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { ListItem } from "./components/ListItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ListItem
+        imageUrl={"https://picsum.photos/id/10/300/300"}
+        title={
+          "Exercitation aute sunt in nulla nisi culpa eiusmod ad minim et."
+        }
+        author={"React News"}
+      />
+      <ListItem
+        imageUrl={"https://picsum.photos/id/20/300/300"}
+        title={"Commodo irure enim nulla irure aliqua nisi sit et."}
+        author={"Japan News"}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
