b96a7426783a325bf1f68ee435086764dd6e4369
Cloud-Xero <hrk.komamiya@gmail.com>
2023-12-29 10:55:49 +0900

    feat: pass parameters

--- react-native/first/screen/ArticleScreen.js
+++ react-native/first/screen/ArticleScreen.js
@@ -1,6 +1,9 @@
 import { SafeAreaView, StyleSheet, Text } from "react-native";
 
-export const ArticleScreen = () => {
+export const ArticleScreen = ({ route }) => {
+  console.log(route);
+  const article = route.params;
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>This is Article Screen.</Text>
