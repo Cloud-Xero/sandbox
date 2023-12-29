da78fd749825462d6be2142d9ff31ba80f3cde07
Cloud-Xero <hrk.komamiya@gmail.com>
2023-12-29 18:04:27 +0900

    feat: persist store

--- react-native/first/App.js
+++ react-native/first/App.js
@@ -6,7 +6,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ClipScreen } from "./screen/ClipScreen";
 import { FontAwesome } from "@expo/vector-icons";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store, persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -50,20 +51,22 @@ const ClipStack = () => {
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Tab.Navigator screenOptions={screenOption}>
-          <Tab.Screen
-            name="HomeTab"
-            component={HomeStack}
-            options={{ headerShown: false, title: "Home" }}
-          />
-          <Tab.Screen
-            name="ClipTab"
-            component={ClipStack}
-            options={{ headerShown: false, title: "Clip" }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={screenOption}>
+            <Tab.Screen
+              name="HomeTab"
+              component={HomeStack}
+              options={{ headerShown: false, title: "Home" }}
+            />
+            <Tab.Screen
+              name="ClipTab"
+              component={ClipStack}
+              options={{ headerShown: false, title: "Clip" }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- react-native/first/store/index.js
+++ react-native/first/store/index.js
@@ -1,8 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import useReducer from "./userSlice";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+  user: useReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: useReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
