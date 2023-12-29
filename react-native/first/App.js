4c6545959ca61771b40a095e06551e5ca759e144
Cloud-Xero <hrk.komamiya@gmail.com>
2023-12-29 09:30:52 +0900

    feat: use React Navigation

--- react-native/first/App.js
+++ react-native/first/App.js
@@ -1,61 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { FlatList, SafeAreaView, StyleSheet } from "react-native";
-import { ListItem } from "./components/ListItem";
-import { useEffect, useState } from "react";
-// import dummyArticles from "./dummies/articles.json";
-import axios from "axios";
-import Constants from "expo-constants";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "./screen/HomeScreen";
 
-const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.expoConfig.extra.newsApiKey}`;
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [articles, setArticles] = useState([]);
-
-  const fetchArticles = async () => {
-    try {
-      const response = await axios.get(URL);
-      // console.log(response.data.articles);
-      setArticles(response.data.articles);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchArticles();
-  }, []);
-
-  const items = articles.map((article, index) => (
-    <ListItem
-      imageUrl={article.urlToImage}
-      title={article.title}
-      author={article.author}
-      key={index.toString()}
-    />
-  ));
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-            imageUrl={item.urlToImage}
-            title={item.title}
-            author={item.author}
-          />
-        )}
-        keyExtractor={(_, index) => index.toString()}
-      >
-        {items}
-      </FlatList>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
---
+++ react-native/first/screen/HomeScreen.js
@@ -0,0 +1,59 @@
+import { FlatList, SafeAreaView, StyleSheet } from "react-native";
+import { ListItem } from "../components/ListItem";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import Constants from "expo-constants";
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.expoConfig.extra.newsApiKey}`;
+
+export const HomeScreen = () => {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      // console.log(response.data.articles);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const items = articles.map((article, index) => (
+    <ListItem
+      imageUrl={article.urlToImage}
+      title={article.title}
+      author={article.author}
+      key={index.toString()}
+    />
+  ));
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(_, index) => index.toString()}
+      >
+        {items}
+      </FlatList>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
