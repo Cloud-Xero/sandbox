7652c0043e0c58ffb91b20f22c53c6deb847aac6
Cloud-Xero <hrk.komamiya@gmail.com>
2023-12-29 15:38:37 +0900

    feat: dispatch action

--- react-native/first/screen/ArticleScreen.js
+++ react-native/first/screen/ArticleScreen.js
@@ -1,11 +1,20 @@
 import { SafeAreaView, StyleSheet, Text } from "react-native";
 import { WebView } from "react-native-webview";
+import { ClipButton } from "../components/ClipButton";
+import { useDispatch } from "react-redux";
+import { addClip } from "../store/userSlice";
 
 export const ArticleScreen = ({ route }) => {
   const { article } = route.params;
+  const dispatch = useDispatch();
+
+  const onPressClip = () => {
+    dispatch(addClip(article));
+  };
 
   return (
     <SafeAreaView style={styles.container}>
+      <ClipButton onPress={onPressClip} enabled={false} />
       <WebView source={{ url: article.url }}></WebView>
     </SafeAreaView>
   );
