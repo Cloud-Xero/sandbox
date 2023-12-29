df0fe1dac132956f57f0889ae9e8eb871d69ed5d
Cloud-Xero <hrk.komamiya@gmail.com>
2023-12-29 12:25:29 +0900

    style: display icons of font awesome

--- react-native/first/App.js
+++ react-native/first/App.js
@@ -4,9 +4,20 @@ import { HomeScreen } from "./screen/HomeScreen";
 import { ArticleScreen } from "./screen/ArticleScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ClipScreen } from "./screen/ClipScreen";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const screenOption = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    if (route.name === "HomeTab") {
+      return <FontAwesome name="home" size={size} color={color} />;
+    } else if (route.name === "ClipTab") {
+      return <FontAwesome name="bookmark" size={size} color={color} />;
+    }
+  },
+});
 
 const HomeStack = () => {
   return (
@@ -24,16 +35,16 @@ const HomeStack = () => {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={screenOption}>
         <Tab.Screen
           name="HomeTab"
           component={HomeStack}
-          options={{ headerShown: false }}
+          options={{ headerShown: false, title: "Home" }}
         />
         <Tab.Screen
           name="ClipTab"
           component={ClipScreen}
-          options={{ headerShown: false }}
+          options={{ headerShown: false, title: "Clip" }}
         />
       </Tab.Navigator>
     </NavigationContainer>
