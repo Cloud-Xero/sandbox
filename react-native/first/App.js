c7f7d9cabb2dab9e065750d4eb4b38ae1badc55b
Cloud-Xero <hrk.komamiya@gmail.com>
2023-12-29 12:06:26 +0900

    feat: display tab

--- react-native/first/App.js
+++ react-native/first/App.js
@@ -2,20 +2,40 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen } from "./screen/HomeScreen";
 import { ArticleScreen } from "./screen/ArticleScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { ClipScreen } from "./screen/ClipScreen";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      ></Stack.Screen>
+      <Stack.Screen name="Article" component={ArticleScreen}></Stack.Screen>
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
+      <Tab.Navigator>
+        <Tab.Screen
+          name="HomeTab"
+          component={HomeStack}
           options={{ headerShown: false }}
-        ></Stack.Screen>
-        <Stack.Screen name="Article" component={ArticleScreen}></Stack.Screen>
-      </Stack.Navigator>
+        />
+        <Tab.Screen
+          name="ClipTab"
+          component={ClipScreen}
+          options={{ headerShown: false }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
